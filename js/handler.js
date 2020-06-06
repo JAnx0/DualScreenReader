d3db4489b837f94fd148bc583d104350c8ea9773
Jannik Schmied <s78712@htw-dresden.de>
2020-06-06 16:14:15 +0200

    hide line when menu opens

--- js/handler.js
+++ js/handler.js
@@ -4,7 +4,7 @@ window.onload = function ()
     document.getElementById('line').style.top = localStorage.getItem("line-position");
 
     // Set line thickness
-    document.getElementById('line').style.height = localStorage.getItem("line-thickness");
+    document.getElementById('lineheader').style.height = localStorage.getItem("line-thickness");
 
     // Set scroll positions
     document.getElementById('left').scrollTop = Number(localStorage.getItem("scroll-pos-left"));
