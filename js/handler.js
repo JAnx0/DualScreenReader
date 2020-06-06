11379f3fed26de56f2fba6cfdf0ddf1f933281c0
decamargo10 <59370581+decamargo10@users.noreply.github.com>
2020-06-07 01:08:54 +0200

    added localStorage for text + file names

--- js/handler.js
+++ js/handler.js
@@ -9,5 +9,10 @@ window.onload = function ()
     // Set scroll positions
     document.getElementById('left').scrollTop = Number(localStorage.getItem("scroll-pos-left"));
     document.getElementById('right').scrollTop = Number(localStorage.getItem("scroll-pos-right"));
-
+    // Set texts
+    document.getElementById("rightOutput").innerHTML = localStorage.getItem("textRight");
+    document.getElementById("leftOutput").innerHTML = localStorage.getItem("textLeft");
+    // Set text names
+    document.getElementById('file_name_right').innerHTML = localStorage.getItem("textRightName");
+    document.getElementById('file_name_left').innerHTML = localStorage.getItem("textLeftName");
 }
